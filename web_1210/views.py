6c69f71a12fc67ee6cd91a67f3a3395042110071
AvisTsai <edcft150089@gmail.com>
2018-12-12 21:03:30 +0800

    second commit

--- web_1210/views.py
+++ web_1210/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def root(request):
     name = request.GET.get("name", "guest")
@@ -11,7 +12,15 @@ def s(request, number):
     return HttpResponse(number **2)
 
 def l(request, num1, num2):
-    result =  ["<li>{}</li>".format(i) for i in range(num1, num2+1)]
-    return HttpResponse("<ul>{}</ul>".format(" ".join(result)))
+    # if num1 < num2:
+    #     _list = range(num1, num2+1)
+    # else:
+    #     _list = reversed(range(num2, num1+1))
 
+    _list = range(10, 0, -1)
+
+    step = -1 if num1>num2 else 1
+    _list = range(num1, num2+step, step)
+    
+    return render(request, "h.html", {"list": _list})
 
